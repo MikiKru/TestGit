34dd982474a33982e95b2483230224e6a5fe27c4
kasia <kgbsz@wp.pl>
2019-02-20 17:45:45 +0100

    Kasia

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Tu Michał");
+        System.out.println("Hola, Kasia");
     }
 }
