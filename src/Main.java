9e32b4b74473286fd8b2688e58f30ecd3b06f127
MikiKru <michal_kruczkowski@o2.pl>
2019-02-20 17:26:03 +0100

    praca michała

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Początek projektu");
         System.out.println("Tu Michał");
+        System.out.println("Moja praca");
+        System.out.println(Math.E);
     }
 }
