f32356472d9518317c4cd52e8881a64c72e3eec4
Karol <karolwroblewicz@gmail.com>
2019-02-20 22:36:53 +0100

    xx4b

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	System.out.println("Kasia ,  Que tall?");
 	
-// Kasia utworzyłem nowy branch karol potrafisz tak?:D
+// Kasia utworzyłem nowy branch karol, potrafisz tak?:D
 // Pokaż co potrafisz i się pochwal!;)
     }
 
