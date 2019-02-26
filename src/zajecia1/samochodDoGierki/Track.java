82886bbafc6cdda1ad8bda9270818cadd2186917
kasia <kgbsz@wp.pl>
2019-02-26 18:50:57 +0100

    Dodanie komentarza do Track

--- src/zajecia1/samochodDoGierki/Track.java
+++ src/zajecia1/samochodDoGierki/Track.java
@@ -1,13 +1,17 @@
 package zajecia1.samochodDoGierki;
-
+// klasa tor, po ktorej przemieszczaja sie samochody,
 public class Track {
-
+    // deklaracja tablicy ktora moze zawierac samochody,
+    // jesli na pozycji jest null to nie ma samochodu,
+    // jesli nie jest null to znjaduje sie tam samochod
     private Car[] cars;
-
+    // konstruktor toru,ktory towrzy tor o podanej dlugosci
     public Track(int size) {
+        // alokacja pamieci na tablice o rozmairze size,
+        // wszystkie lelementy tablicy domyslnie sa null
         this.cars = new Car[size];
     }
-
+    // implementacja zamiany tor na string w celu jego wyswietlnia
     @Override
     public String toString() {  // toString dziedziczy po klasie object,konwertuje na string
         String viewcar = "";
@@ -20,14 +24,15 @@ public class Track {
         }
         return viewcar;
     }
-
+    // metoda ustawiajaca samochod na danej pozycji
     public void setCarAt(int position, Car car) {
         cars[position] = car;
     }
-
+    // metoda sprawdzjaca czy na pozycji jest samochod
     public boolean isCarAt(int position) {
         return cars[position] != null;
 
+        // ten blok zakomentowany robi to samo,
 /*        if (cars[position] == null) {
             return false;
         } else {
