76c759bbca67c1db641b5e27c407fbe92d37ba69
kasia <kgbsz@wp.pl>
2019-02-26 18:07:00 +0100

    Add code to track, Main

--- src/zajecia1/samochodDoGierki/Main.java
+++ src/zajecia1/samochodDoGierki/Main.java
@@ -1,16 +1,18 @@
 package zajecia1.samochodDoGierki;
-
+//
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 /*        Wheel[] wheels = {new Wheel(), new Wheel(), new Wheel(), new Wheel()};
         Car car = new Car("red", wheels, new Engine(), 10);
         car.move();*/
         Track tor = new Track(100);
         Car car1 = new Car(tor, 0);
         Car car2 = new Car(tor,10);
-        for(int i=0; i < 10; i++){
-            car1.move(1);
-        }
         System.out.println(tor);
+        for(int i=0; i < 10; i++){
+            Thread.sleep(500);
+            car1.move(1);
+            System.out.println(tor);
+        }
     }
 }
--- src/zajecia1/samochodDoGierki/Track.java
+++ src/zajecia1/samochodDoGierki/Track.java
@@ -4,13 +4,22 @@ public class Track {
 
     private Car[] cars;
 
-
     public Track(int size) {
         this.cars = new Car[size];
-
-
     }
 
+    @Override
+    public String toString() {  // toString dziedziczy po klasie object,konwertuje na string
+        String viewcar = "";
+        for (int i = 0; i < cars.length; i++) {
+            if (cars[i] == null) {
+                viewcar += "_";
+            } else {
+                viewcar += "C";
+            }
+        }
+        return viewcar;
+    }
 
     public void setCarAt(int position, Car car) {
         cars[position] = car;
