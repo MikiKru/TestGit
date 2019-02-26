4f84786c52b4d86bce95e93ff927057baf505093
kasia <kgbsz@wp.pl>
2019-02-26 19:03:48 +0100

    Wymiana implementacji toString na Stringbuilder w klasie Track

--- src/zajecia1/samochodDoGierki/Track.java
+++ src/zajecia1/samochodDoGierki/Track.java
@@ -1,33 +1,40 @@
 package zajecia1.samochodDoGierki;
+
 // klasa tor, po ktorej przemieszczaja sie samochody,
 public class Track {
     // deklaracja tablicy ktora moze zawierac samochody,
     // jesli na pozycji jest null to nie ma samochodu,
     // jesli nie jest null to znjaduje sie tam samochod
     private Car[] cars;
+
     // konstruktor toru,ktory towrzy tor o podanej dlugosci
     public Track(int size) {
         // alokacja pamieci na tablice o rozmairze size,
         // wszystkie lelementy tablicy domyslnie sa null
         this.cars = new Car[size];
     }
+    //stringBuilder - klasa, 1, tworzenie obiektu,
+
     // implementacja zamiany tor na string w celu jego wyswietlnia
     @Override
     public String toString() {  // toString dziedziczy po klasie object,konwertuje na string
-        String viewcar = "";
+        StringBuilder viewBuilder = new StringBuilder();
+
         for (int i = 0; i < cars.length; i++) {
             if (cars[i] == null) {
-                viewcar += "_";
+                viewBuilder.append("_");
             } else {
-                viewcar += "C";
+                viewBuilder.append("C");
             }
         }
-        return viewcar;
+        return viewBuilder.toString();
     }
+
     // metoda ustawiajaca samochod na danej pozycji
     public void setCarAt(int position, Car car) {
         cars[position] = car;
     }
+
     // metoda sprawdzjaca czy na pozycji jest samochod
     public boolean isCarAt(int position) {
         return cars[position] != null;
